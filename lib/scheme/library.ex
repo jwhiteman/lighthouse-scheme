9a3677191e66fc3d1a09b3d78e6a73d870e9e7a0
Jim Whiteman <jimtron9000@gmail.com>
2015-07-06 21:12:51 -0700

    make library regex more readable.

--- lib/scheme/library.ex
+++ lib/scheme/library.ex
@@ -1,9 +1,10 @@
 defmodule Scheme.Library do
   @libs_directory "lib/scheme/lib"
   @master "library"
+  @extension "scm"
 
   def load do
-    load("#{@libs_directory}/#{@master}.scm")
+    load("#{@libs_directory}/#{@master}.#{@extension}")
   end
 
   def load(filename) do
@@ -16,12 +17,17 @@ defmodule Scheme.Library do
 
   def read(filename) do
     case look_in_standard_lib?(filename) do
-      true  -> File.read("#{@libs_directory}/#{filename}.scm")
+      true  -> File.read("#{@libs_directory}/#{filename}.#{@extension}")
       false -> File.read(filename)
     end
   end
 
   def look_in_standard_lib?(filename) do
-    String.match?(filename, ~r/^[^\/]+(?<!\.scm)$/)
+    String.match? filename, ~r/
+      ^
+        [^\/]          # no match if filename has any slashes
+        +(?<!\.scm)    # no match if filename ends in .scm
+      $
+    /x
   end
 end
