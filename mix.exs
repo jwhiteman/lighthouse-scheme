8492c3755dedaea7e1227bf8f53449ee1a030c48
Jim Whiteman <jimtron9000@gmail.com>
2016-01-12 16:14:17 -0800

    Bump Elixir version to 1.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scheme.Mixfile do
   def project do
     [app: :lighthouse,
      version: "0.2.0",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: escript_config,
