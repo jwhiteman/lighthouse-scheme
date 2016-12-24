60f58bac80d80ef723a2d3b579a0ccb8647da414
Jim Whiteman <jwhiteman@uplandsoftware.com>
2016-12-23 18:58:56 -0800

    Upgrade supported Elixir to 1.3.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scheme.Mixfile do
   def project do
     [app: :lighthouse,
      version: "0.2.0",
-     elixir: "~> 1.1.1",
+     elixir: "~> 1.3.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: escript_config,
